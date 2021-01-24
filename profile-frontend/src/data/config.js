a9f024cbf8f25c00edd2483bdc54ec210f7da014
vd8413 <vd8413@srmist.edu.in>
2021-01-24 21:55:28 +0000

    yolo

--- profile-frontend/src/data/config.js
+++ profile-frontend/src/data/config.js
@@ -1,10 +1,10 @@
 
 module.exports = {
   // TODO: change your profile information here
-  name: "Ashok Kumar",
+  name: "Ved Prakash Dubey",
   greeting: "Hey 👋",
-  greetingDescription: "I'm Ashok Kumar and I'm a Software Engineer!",
-  githubUrl: "https://github.com/ashokkumar95",
-  linkedinUrl: "https://linkedin.com/ashokkumar1",
+  greetingDescription: "I'm Ved Prakash Dubey and I'm a Data Scientist!",
+  githubUrl: "https://github.com/VedPDubey",
+  linkedinUrl: "https://www.linkedin.com/in/ved-prakash-dubey-68466b18b/",
   cvLink: "https://docs.google.com/document/d/1E1234561NMyQA67890ygda7abcdefghijs1jBp7HFI0/",
 };
